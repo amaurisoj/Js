26cfc24b78b2b4382b568a4f5111568bb621e76f
amaurisoj <amaurisantos356@gmail.com>
2021-05-14 18:15:32 -0300

    first

---
+++ aula01/script.js
@@ -0,0 +1,18 @@
+//DESAFIO 1//
+let nome = "Amauri"
+let idade = 25
+let ano = 2021
+let nascimento = ano - idade
+let number1 = 1
+let number5 = 5
+let souHumano = true
+let isTrue = "verdadeiro"
+let isFalse = "falso"
+if ( souHumano == true) {
+    console.log( isTrue)
+}
+console.log( `Meu nome é ${nome} e meu ano de nascimento é ${nascimento} fazendo eu ter ${idade} anos em ${ano}`) 
+console.log (number1 + number5) 
+console.log( 3 > 2 )
+
+//DESAFIO 2//
