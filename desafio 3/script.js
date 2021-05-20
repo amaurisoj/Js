1c77eecb349dbde46bf8af5f49c386a1083be5da
amaurisoj <amaurisantos356@gmail.com>
2021-05-19 23:13:10 -0300

    desafio 3

---
+++ desafio 3/script.js
@@ -0,0 +1,27 @@
+//DESAFIO 3//
+
+
+for ( let A = 0; A <= 10; A++ ){
+    console.log(A)
+}
+
+console.log("-------------------------------------------")
+let m = 10
+
+while( m >= 1){
+    console.log(m)
+    m--
+}
+console.log("-------------------------------------------")
+
+for( let I = 1; I <= 100; I+=2){
+    console.log(I)
+}
+
+console.log("-------------------------------------------")
+
+for( let P = 0; P <= 100; P+=2){
+    console.log(P)
+}
+
+//DESAFIO 3//
